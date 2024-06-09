6853ac523d0fb9a21975973112a1552280e4c734
takenoshin <takenoshin@yahoo.com>
2024-06-09 10:53:39 +0800

    Clean commit

---
+++ spin-and-win-csgo/OLD_1/languages/jp.php
@@ -0,0 +1,59 @@
+<?php
+
+$meta_title = 'MEC Spin＆Winゲームで、運を試してみよう！';
+$meta_description = 'ホイールを回して、特別賞品とそのほか景品を獲得するチャンスを見逃すな！ESportsクラブに参加して、無料でホイールをスピンして、運試しに挑戦すべし！';
+$subtitle = 'スマホやガジェットを獲得 そのほかの賞品も！';
+$play_btn = '今すぐプレイ';
+$spin_txt = '今すぐプレイ';
+$responsible_link ='https://help.m88.com/responsibleGaming_jp.asp';
+$exclusion_link ='https://www.m88.com/~/static/standalone-pages/promotions/gameexclusion_list.ja-JP';
+$m88terms_link ='https://help.m88.com/terms-of-use/terms-conditions.ja-JP.asp';
+$text_tnc = '利用条件';
+$text_mechanics = 'Mechanics';
+$text_m88commited ='M88 Mansionは';
+$text_responsiblegaming ='責任あるゲーム</a>を支援することを約束します。';
+
+$tnc = <<<HTML
+    <div class="uk-modal-header uk-text-center uk-padding-small">
+        <h5 class="uk-modal-title uk-margin-small">利用条件</h5>
+    </div>
+
+    <div class="uk-modal-body" uk-overflow-auto>
+
+        <ol>
+            <li class="uk-margin-small-bottom">本プロモーションは、2023年6月1日 01:00:00 から2024年1月1日 24:59:00 (日本時間) まで、M88 Mansionのすべての新規メンバーに適用されます。</li>
+            <li class="uk-margin-small-bottom">プロモーションは、以下の条件を満たすメンバーが対象となります:<br>・プロモーション期間中にM88マンションアカウントを登録した新しいメンバー<br>・共有されたリンクを使ってアカウントを作成された<br>・一度もボーナスを受け取っていない<br>・一度も送金をしていない</li>
+            <li class="uk-margin-small-bottom">対象プレーヤーは、定額ボーナスまたは、その他の賞品が受け取れます。賞品は、M88マンションアカウントの作成後、および初期送金後、24~48時間または、次営業日に配布されます。</li>
+            <li class="uk-margin-small-bottom">定額ボーナス: USD5 / MYR20 / IDR70 / THB160 / VND115 / CNY30 / INR400 / KRW7000, USD20 / MYR80 / IDR280 / THB640 / VND460 / CNY140 / INR1400 / KRW2800。ロールオーバー条件は、eスポーツで3倍の合計額をベットする必要があります。</li>
+            <li class="uk-margin-small-bottom">複数の定額ボーナスを獲得した場合は、最高額のボーナスのみが付与されます。</li>
+            <li class="uk-margin-small-bottom">物理賞品: 物理賞品の価値50%までボーナスと交換できます。物理賞品をご希望の場合、24時間以内にカスタマーサービスに連絡ください。連絡がない場合は、自動的にボーナスが配布されます。</li>
+            <li class="uk-margin-small-bottom">MEC抽選チケットおよび335CGのサイン入りシャツは、ボーナスおよびキャッシュと交換できません。</li>
+            <li class="uk-margin-small-bottom">MEC抽選チケットは、次回のMEC抽選会のエントリーとなります。抽選会の詳細は、MECサイトをご覧ください。</li>
+            <li class="uk-margin-small-bottom">物理賞品50%交換対象: ゲーミングPC USD1000 MIR4000 / IDR14000 / THB32000 / VND23000 / CNY7000 / INR8000 / KRW1400000. スペシャルグローブI Crimson Web スキン、 USD125 / MYR500 / IDR1450 / THB4000 / VND2875 / CNY875 / INR10000 / KRW175000. $10のSteamギフトカード： USD5 / MYR20 / IDR70 / THB160 / VND115 / CNY35 / INR400 / KRW7000. ボーナスのロールオーバー条件は、eスポーツで1倍の合計額をベットする必要があります。</li>
+            <li class="uk-margin-small-bottom">ゲーミングPC、スペシャルグローブI Crimson Web スキン、またはボーナス交換の受取には、最低、USD30 MYR120 / IDR420 / THB960 / VND690 / CNY210 / INR2400 / KRW36000の入金額が必要となります。ロールオーバー条件は、eスポーツで6倍の合計額をベットする必要があります。</li>
+            <li class="uk-margin-small-bottom">各ボーナスの有効期限は14日間です。有効期限までにロールオーバーを達成できなかった場合、ボーナス額とその勝利額は没収されます。 <a href="$exclusion_link" target="_blank">こちらをクリックして除外ゲームを確認してください。</a></li>
+            <li class="uk-margin-small-bottom">ボーナスの残高がMYR1未満となった場合、ロールオーバー条件は達成したとみなされます。</li>
+            <li class="uk-margin-small-bottom">最高出金額+ボーナスはUSD88 / MYR370 / IDR1200 / THB2900 / VND2000 / CNY616 / INR7000 / KRW123200です。超過額は無効となります。記載されている最高出金額は、物理賞品の交換には適用されません。</li>
+            <li class="uk-margin-small-bottom">特に記載がない限り、本ボーナスはM88の他のプロモーションと併用することはできません。</li>
+            <li class="uk-margin-small-bottom">無効になったベット、賭けた結果が引き分けになったベットと、シングルベット、マルチベットで組んだ1つでも、デシマルオッズで1.50未満（マレーオッズ 0.50、香港オッズ 0.50、インドーオッズ -2.00）のベットはロールオーバーに加算されません。　</li>
+            <li class="uk-margin-small-bottom">進行中のロールオーバーがある場合は出金は処理されません。ロールオーバーの免除をご希望の場合は、カスタマーサポートまでお問い合わせください。</li>
+            <li class="uk-margin-small-bottom">M88 Mansion は、いつでもプロモーションを変更、更新、修正、中止する権利を有します。</li>
+            <li class="uk-margin-small-bottom"><a href="$m88terms_link" target="_blank">プロモーションの一般利用規約が適用されます。</a></li>
+
+        </ol>
+
+    </div>
+
+HTML;
+
+$text_prizes = '景品';
+$text_viewprizes = '景品を見る';
+$prize_1 ='I5/16GB/RTX3060 ゲーミングPC';
+$prize_2 ='スペシャリストグローブ | クリムゾン・ウェブ・スキン';
+$prize_3 ='USD 20 フリーベット';
+$prize_4 ='USD 5フリーベット＋$10Steamギフトカード';
+$prize_5 ='USD 5フリーベット＋335CG eスポーツユニフォーム';
+$prize_6 ='USD 5フリーベット＋50 M88 eスポーツ抽選チケット';
+$prize_7 ='USD 5 フリーベット';
+
+
---
+++ spin-csgo/index.php
@@ -0,0 +1,298 @@
+﻿<?php
+
+$domain = $_SERVER['SERVER_NAME'];
+
+// domains and config
+include_once('config.php');
+
+// ready for langauge verification
+$site = $trackingCode[$domain];
+unset($site['affId']);
+$languages = array_keys($site);
+
+// set language default first array key
+$language = isset($_GET['lang']) ? $_GET['lang'] : $languages[0];
+// verify exist in array language
+if ( !in_array($language, $languages)){
+
+	// header refresh remove lang
+	// $language = $languages[0];
+
+	$filename = str_replace('index.php', '', $_SERVER['PHP_SELF']);
+	if (!headers_sent())
+		header('Location: '.$filename);
+	else {
+		echo '<script type="text/javascript">';
+		echo 'window.location.href = \''.$filename.'\';';
+		echo '</script>';
+		echo '<noscript>';
+		echo '<meta http-equiv="refresh" content="0;url=\''.$filename.'\'" />';
+		echo '</noscript>';
+	}
+	exit();
+}
+
+include_once('languages/' . $language . '.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="<?php echo $meta_description; ?>">
+	<title><?php echo $meta_title; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- JS FILES REGISTRATION FORM -->
+	<script type="text/javascript" src="https://api.m88.services/signup/assets/js/build-script-regform.js?load=jquery,jquery-validation,uikit,uikit-icons,iconselectbox&123123123"></script>
+
+	<script type="text/javascript" src="js/scroller.js"></script>
+	<!-- <script src="js/additional-methods.js"></script> -->
+	<link rel="icon" href="img/favicon.ico">
+
+	<!-- CSS FILES -->
+	<link rel="stylesheet" type="text/css" href="https://api.m88.services/signup/assets/uikit-3.16.14/css/uikit.min.css">
+
+	<link rel="stylesheet" href="css/custom.css?a11">
+	<!-- CSS FILES REGISTRATION FORM -->
+	<link rel="stylesheet" type="text/css" href="css/registration-form.css?a3">
+
+
+</head>
+<body class="registered" uk-height-viewport> <!-- toggle classes 'registered' & 'unregistered' -->
+
+	<div class="parallax-wrapper">
+		<div class="layer-3" data-mouse-parallax="0.03"></div>
+		<div class="layer-2"></div>
+	</div>
+
+	<header class="uk-position-top uk-position-small reset-position">
+	    <div class="uk-grid uk-child-width-expand" uk-grid>
+	        <div class="uk-flex uk-flex-left">
+						<img class="" src="img/m88logo.png" uk-img/>
+	        </div>
+	        <div class="uk-flex uk-flex-right">
+					  <?php
+					  if ( sizeof($site) > 1 ) : ?>
+						<div class="language-selector uk-flex uk-flex-right">
+
+							<button class="uk-button uk-button-default language-button uk-text-uppercase text-dark" type="button">
+								<span class="icon-flag fl-<?php echo $language; ?>"></span><?php echo $language; ?><span uk-icon="icon: triangle-down"></span>
+							</button>
+
+							<div class="lang-area-outer dropdown-dark" uk-dropdown>
+
+							  <ul class="uk-nav uk-dropdown-nav">
+								<?php
+								foreach ($languages as $key => $value) { 	
+								  $active = ($language === $value ) ? 'uk-active' : '';
+								  echo '<li class="'. $active .' uk-margin-remove uk-text-uppercase">'
+									. '<a lang="'. $value .'" href="?lang='. $value .'">'
+									  . '<span class="icon-flag fl-'. $value .'"></span>'
+									  . $value . '</a></li>';
+								} ?> 
+							  </ul>
+							</div>
+
+						</div>
+					  <?php endif; ?>
+	        </div>
+	    </div>
+	</header>
+
+  <div id="container" class="main-container uk-flex uk-flex-center uk-flex-middle" uk-height-viewport>
+
+		<div class="spinwheel-container uk-flex uk-flex-center uk-flex-wrap-around uk-text-center">
+
+			<div>
+				<div class="logo"><img src="img/logo-<?php echo $language; ?>.webp" /></div>
+				
+				<div class="wheelContainer">
+					<svg class="wheelSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" text-rendering="optimizeSpeed" preserveAspectRatio="xMidYMin meet">
+						<defs>
+							<filter id="shadow" x="-100%" y="-100%" width="550%" height="550%">
+								<feOffset in="SourceAlpha" dx="0" dy="0" result="offsetOut"></feOffset>
+								<feGaussianBlur stdDeviation="9" in="offsetOut" result="drop" />
+								<feColorMatrix in="drop" result="color-out" type="matrix" values="0 0 0 0   0
+								0 0 0 0   0
+								0 0 0 0   0
+								0 0 0 .3 0" />
+								<feBlend in="SourceGraphic" in2="color-out" mode="normal" />
+							</filter>
+						</defs>
+						<g class="mainContainer">
+							<g class="wheel">
+							</g>
+						</g>
+						<g class="wheelOutline" />
+						<g class="pegContainer" opacity="0">
+							<path class="peg" fill="#EEEEEE" d="M22.139,0C5.623,0-1.523,15.572,0.269,27.037c3.392,21.707,21.87,42.232,21.87,42.232 s18.478-20.525,21.87-42.232C45.801,15.572,38.623,0,22.139,0z" />
+						</g>
+						<g class="valueContainer" />
+					</svg>
+					<div class="cwheelOutline"></div>
+							<div class="winning-indicator"></div>
+					<div class="cwheel-arrow"></div>
+					<div class="cwheel-innerCircle"></div>
+					<div class="toast" style="display: none">
+						<div class="toast-inner"><p></p></div>
+						<div class="spin-left" data-txt="txt2"></div>
+					</div>
+					<!-- <img src="img/game/dealer.webp" class="dealer" /> -->
+				</div>
+
+				<div class="area-txt txt-1">asdasdasd</div>
+				<div class="area-cta">
+					<button class="btn spinBtn" data-spinning-txt="spinning-txt"><span class="loader"></span><span><?php echo $spin_txt; ?></span></button>
+					<button class="btn btn-register" data-txt="txt4"></button><br>
+					<button class="btn viewprizes " href="#viewPrizes" uk-toggle><?php echo $text_viewprizes; ?></button>
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+				
+  <footer class="footer uk-light uk-text-center uk-padding-small uk-position-bottom">
+	  <span class="uk-text-small">
+	  	<a href="#popup-tnc" class="tnc-link" uk-toggle><?php echo $text_tnc;?></a> • <?php echo $text_m88commited;?> <a href="<?php echo $responsible_link; ?>" target="_blank" class="responsible-link"><?php echo $text_responsiblegaming;?></a>
+    </span>
+  </footer>
+
+
+
+	<!-- CONGRATS MODAL -->
+   <div id="popup-congrats" class="popup-congrats uk-flex-center uk-text-left uk-border-rounded" uk-modal="stack: true" bg-close="false"esc-close="false">
+   	<div class="bg-ray"></div>
+	  <div class="uk-modal-dialog uk-margin-auto-vertical uk-text-center uk-width-auto" uk-overflow-auto>
+	    <div class="uk-modal-header uk-padding-remove">
+				<img class="congrats-header" src="" >
+			</div>
+	    <div class="uk-modal-body">
+		  	<h4 class="uk-text-uppercase text-white uk-margin-remove" data-txt="txt7"></h4>
+				<h3 id="divDynamicResult" class="text-yellow"></h3>
+			</div>
+	    <div class="uk-modal-footer uk-padding-remove">
+				<a class="uk-button btn claim-btn" uk-toggle="target: #registration-form-modal" data-txt="txt10"></a>
+			</div>
+		</div>
+	</div>
+	<!-- END CONGRATS MODAL -->
+
+	<!-- T&C MODAL -->
+   <div id="popup-tnc" class="popup-tnc uk-flex-top uk-text-left " uk-modal="stack: true" bg-close="true" >
+	  <div class="uk-modal-dialog uk-margin-auto-vertical uk-border-rounded" uk-overflow-auto>
+		  <button class="uk-modal-close-default" type="button" uk-close></button>
+			<?php echo $tnc; ?>
+		</div>
+	</div>
+	<!-- END T&C MODAL -->
+
+	<!-- MECHANICS MODAL -->
+	<div id="popup-mechanics" class="popup-mechanics modal" uk-modal="stack: true" bg-close="true">
+		<div class="uk-modal-dialog uk-margin-auto-vertical" >
+			<?php echo $mechanics; ?>
+		</div>
+	</div>
+	<!-- END MECHANICS MODAL -->
+
+	<!-- VIEW PRIZES OFFCANVAS -->
+	<div id="viewPrizes" uk-offcanvas>
+    <div class="prizes-container uk-offcanvas-bar">
+
+      <button class="uk-offcanvas-close" type="button" uk-close></button>
+      <h3><?php echo $text_prizes; ?></h3>
+
+				<div class="prize-card" uk-grid>
+			    <div class="uk-width-auto"><img src="img/prize-1.png" class=""></div>
+			    <div class="uk-width-expand uk-flex uk-flex-middle"><?php echo $prize_1; ?></div>
+				</div>
+
+				<div class="prize-card" uk-grid>
+			    <div class="uk-width-auto"><img src="img/prize-2.png" class=""></div>
+			    <div class="uk-width-expand uk-flex uk-flex-middle"><?php echo $prize_2; ?></div>
+				</div>
+
+				<div class="prize-card" uk-grid>
+			    <div class="uk-width-auto"><img src="img/prize-3.png" class=""></div>
+			    <div class="uk-width-expand uk-flex uk-flex-middle"><?php echo $prize_3; ?></div>
+				</div>
+
+				<div class="prize-card" uk-grid>
+			    <div class="uk-width-auto"><img src="img/prize-4.png" class=""></div>
+			    <div class="uk-width-expand uk-flex uk-flex-middle"><?php echo $prize_4; ?></div>
+				</div>
+
+				<div class="prize-card" uk-grid>
+			    <div class="uk-width-auto"><img src="img/prize-5.png" class=""></div>
+			    <div class="uk-width-expand uk-flex uk-flex-middle"><?php echo $prize_5; ?></div>
+				</div>
+
+				<div class="prize-card" uk-grid>
+			    <div class="uk-width-auto"><img src="img/prize-6.png" class=""></div>
+			    <div class="uk-width-expand uk-flex uk-flex-middle"><?php echo $prize_6; ?></div>
+				</div>
+
+
+				<div class="prize-card" uk-grid>
+			    <div class="uk-width-auto"><img src="img/prize-7.png" class=""></div>
+			    <div class="uk-width-expand uk-flex uk-flex-middle"><?php echo $prize_7; ?></div>
+				</div>
+
+    </div>
+  </div>
+
+
+	<div id="registration-container"></div>
+
+
+	<script type="text/javascript">
+	  const promoName = '<?php echo $promoName;?>';
+	  let trackingCode = <?php echo json_encode($trackingCode[$domain]);?>;
+	  let bonusValue = 0;
+	  const defaultLang = '<?php echo $language; ?>';
+	  // global variable 
+	  var lang = '<?php echo $language;?>';
+	</script>
+	
+	<script src="js/scripts.js?a5"></script>
+	<script src="js/spin-libs.js"></script>
+	<script src='js/spinwheel.js?a1'></script>
+	<script src="js/script-api.js?a1"></script>
+
+	<script type="text/javascript" src="https://api.m88.services/signup/assets/js/scripts.js?v2.70"></script>
+	<script type="text/javascript">
+	  jQuery(document).ready(function() {
+			$('#registration-container').RegistrationForm({
+			  'mode' : '',
+			  'banner_mobile': 'img/reg-mobile-<?php echo $language;?>.webp',
+			  'banner_desktop': 'img/reg-img.webp',
+			  'languages': <?php echo json_encode($languages);?>,
+			  'include_inr': false,
+			  // 'currency': ['cny','usd'],
+			  'lock-mobile': true,
+			  'default-language': '<?php echo $languages[0];?>',
+			  'promo': null,
+			  'affiliate_id': null,
+			  'campaign_name': null
+			});
+
+			$(".registered").mousemove(function(e) {
+			  var x = e.pageX - $(this).offset().left - $(this).width() / 2;
+			  var y = e.pageY - $(this).offset().top - $(this).height() / 2;
+			  
+			  $("*[data-mouse-parallax]").each(function() {
+			    var factor = parseFloat($(this).data("mouse-parallax"));
+			    x = -x * factor;
+			    y = -y * factor;
+
+			    $(this).css({ transform: "translate3d( " + x + "px, " + y + "px, 0 )" });
+			  });
+			});
+
+	  });
+	</script>
+</body>
+</html>
