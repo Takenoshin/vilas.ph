88c453fa5f5a23ec27ca13459843687c8e6a21c5
takenoshin <takenoshin@yahoo.com>
2025-03-13 14:21:28 +0800

    MVP for 1 Case Study

---
+++ previous-works/spin-and-win-csgo/OLD_1/languages/my-cn.php
@@ -0,0 +1,58 @@
+<?php
+
+$meta_title = '旋转&幸运转转乐 试试你的好手气';
+$meta_description = '准备好大显身手了吗？立即开始转动吧！有好多奖品等着您，别忘了加入我们电竞具乐部，还有更多意想不到的惊喜等着您。';
+$subtitle = '赢取独家奖品';
+$play_btn = '立即旋转';
+$spin_txt = '立即旋转';
+$responsible_link ='https://help.m88.com/responsibleGaming_cs.asp';
+$exclusion_link ='https://www.m88.com/~/static/standalone-pages/promotions/gameexclusion_list.zh-CN';
+$m88terms_link ='https://help.m88.com/terms-of-use/terms-conditions.zh-CN.asp';
+$text_tnc = '条款和条件';
+$text_mechanics = '活动介绍';
+$text_m88commited ='明陞M88 Mansion致力于支持';
+$text_responsiblegaming ='Responsible Gaming';
+
+$tnc = <<<HTML
+    <div class="uk-modal-header uk-text-center uk-padding-small">
+        <h5 class="uk-modal-title uk-margin-small">条款与规则</h5>
+    </div>
+
+    <div class="uk-modal-body" uk-overflow-auto>
+
+        <ol>
+            <li class="uk-margin-small-bottom">本优惠活动适用2023年6月1日至2023年12月31日所有明陞M88新玩家。</li>
+            <li class="uk-margin-small-bottom">此促销活动邀请对象为，活动期间内尚未领取任何奖励及尚未存款之会员新玩家。</li>
+            <li class="uk-margin-small-bottom">符合条件的玩家于明陞M88完成注册后并存款30马币，系统于24-48小时内发送您奖金或其他奖项。</li>
+            <li class="uk-margin-small-bottom">奖金可使用在电竞游戏中，规则如下：20马币、80马币 (电竞游戏中3倍流水)</li>
+            <li class="uk-margin-small-bottom">若您赢取多次奖金，系统将自动选择一次最高奖金。</li>
+            <li class="uk-margin-small-bottom">奖品的部分可转换为奖金，最高可转换其价值的 50%，获奖者如需领取实物奖品，需在 24 小时内联系客服。如没有联系客服，该项目将被自动转换为奖金。</li>
+            <li class="uk-margin-small-bottom">MEC抽奖券和335CG队服不可兑换成奖金或现金。</li>
+            <li class="uk-margin-small-bottom">请查看MEC(明陞电竞俱乐部)网站了解更多关于抽奖赠品的详情。</li>
+            <li class="uk-margin-small-bottom">实物奖励50%兑换：游戏PC：可转换奖金4000马币,专业手套 | 深红之网 皮肤：可钻换奖金5000马币, STEAM 10美金充值卡：可转换奖金20马币, 转换奖金后必须在电竞游戏里完成1倍流水</li>
+            <li class="uk-margin-small-bottom">如需将奖品转换为奖金，请完成最低存款120马币，请于电竞游戏中达成6倍流水。</li>
+            <li class="uk-margin-small-bottom">奖金请在电竞游戏中于14天内达成流水要求，以防止奖金和获胜金额被取消。其余类别游戏皆排除在外，<a href="$exclusion_link" target="_blank">单击此处查看包含和排除的游戏列表。</a></li>
+            <li class="uk-margin-small-bottom">若会员账号余额低于1马币，系统将清除未完成的有效投注。</li>
+            <li class="uk-margin-small-bottom">奖金加红利的最高提款金额为370马币，超出部分系统自动取消。（奖品转换为奖金不包含在此规则中）</li>
+            <li class="uk-margin-small-bottom">本档活动奖金，不可与其他明陞M88优惠活动同时使用。</li>
+            <li class="uk-margin-small-bottom">优惠活动期间内，仅计入所有已结算的注单。 所有平局/退款/拒绝/无效/取消的投注，以及任何低于1.50 赔率的投注（马来盘赔率0.50 ； 香港盘赔率0.50； 印尼盘赔率-2.00）等将不计算在有效投注内。</li>
+            <li class="uk-margin-small-bottom">如果您的账户仍有未完成的有效流水要求，您将无法提款。请联系在线客服办理撤除/取消/豁免流水要求即可。</li>
+            <li class="uk-margin-small-bottom">明陞M88保留随时修改、暂停或取消推广活动的权利，恕不另行通知。</li>
+            <li class="uk-margin-small-bottom"><a href="$m88terms_link" target="_blank">一般条款及规则应用于此优惠。</a></li>
+        </ol>
+
+    </div>
+
+HTML;
+
+
+$text_prizes = '奖品';
+$text_viewprizes = '查看奖品';
+$prize_1 ='I5/16GB/RTX3060 游戏PC';
+$prize_2 ='专业手套 | 深红之网皮肤';
+$prize_3 ='免费下注 80 马币';
+$prize_4 ='免费下注 20 马币 + STEAM 10美金充值卡';
+$prize_5 ='免费下注 20 马币 + 335CG 电竞队服';
+$prize_6 ='免费下注 20 马币 + 50张 M88 电竞彩票';
+$prize_7 ='免费下注 20 马币';
+
---
+++ previous-works/spin-csgo/languages/in.php
@@ -0,0 +1,58 @@
+<?php
+
+$meta_title = 'Try your luck with MEC Spin & Win game! ';
+$meta_description = 'Get ready to spin the wheel for a chance to win exciting prizes and more! Join our Esports Club now and spin the wheel to test your luck for free! ';
+$subtitle = 'Win Exclusive Prizes!';
+$play_btn = 'PLAY NOW!';
+$spin_txt = 'SPIN NOW!';
+$responsible_link ='https://help.m88.com/responsibleGaming_en.asp';
+$exclusion_link ='https://www.m88.com/~/static/standalone-pages/promotions/gameexclusion_list.en-IN';
+$m88terms_link ='https://help.m88.com/terms-of-use/terms-conditions.en-IN.asp';
+$text_tnc = 'Terms & Conditions';
+$text_mechanics = 'Mechanics';
+$text_m88commited ='M88 Mansion is committed to supporting';
+$text_responsiblegaming ='Responsible Gaming';
+
+$tnc = <<<HTML
+    <div class="uk-modal-header uk-text-center uk-padding-small">
+        <h5 class="uk-modal-title uk-margin-small">Terms & Conditions</h5>
+    </div>
+
+    <div class="uk-modal-body" uk-overflow-auto>
+
+        <ol>
+            <li class="uk-margin-small-bottom">This promotion is applicable to all new M88 Mansion players from 01/08/2022 00:00:00 to 31/12/2022 23:59 (GMT+8).</li>
+            <li class="uk-margin-small-bottom">This promotion is open to NEW MEMBERS who created their M88 Mansion accounts during the promotion period, created their account using the given links, and who have not claimed any bonus or placed a first deposit.</li>
+            <li class="uk-margin-small-bottom">Eligible players are entitled to receive a FIXED BONUS or other prizes, 24-48 hours, or the next business day upon M88 Mansion account creation and first deposit.</li>
+            <li class="uk-margin-small-bottom">FIXED BONUS: ₹400, ₹1600 3x rollover in Esports.</li>
+            <li class="uk-margin-small-bottom">For multiple FIXED BONUSES won, only the highest FIXED BONUS will be awarded.</li>
+            <li class="uk-margin-small-bottom">PHYSICAL PRIZES: These prizes may be converted to a bonus with up to 50% of their value. Winners need to contact Customer Service within 24 hours if they wish to receive the physical prize. If not, the item will be converted to a bonus instead.</li>
+            <li class="uk-margin-small-bottom">The MEC Raffle Tickets and 335CG shirts are not convertible to bonus or cash.</li>
+            <li class="uk-margin-small-bottom">For the MEC Raffle Tickets, these will be entered in the next MEC Raffle Draw Giveaway. Please check the MEC site for more details on the Raffle Giveaway Draw.</li>
+            <li class="uk-margin-small-bottom">PHYSICAL PRIZES 50% Conversion: Gaming PC: ₹80000, Specialist Gloves | Crimson Web Skin: ₹100000, $10 Steam Gift Card: ₹400. These bonuses need to to be rolled over x1 in Esports.</li>
+            <li class="uk-margin-small-bottom">To claim the Gaming PC, Specialist Gloves | Crimson Web Skin, $10 Steam Gift Card, or the converted bonus from these items, a minimum deposit of ₹2400 is required with an x6 rollover in Esports.</li>
+            <li class="uk-margin-small-bottom">The rollover must be fulfilled within 14 days in Esports to prevent forfeiture of the bonus and the winning amount. <a href="$exclusion_link" target="_blank">Click here to see the Rollover Contribution games list.</a></li>
+            <li class="uk-margin-small-bottom">The bonus will be automatically redeemed if the balance falls to below ₹ 1.</li>
+            <li class="uk-margin-small-bottom">The maximum withdrawal of winnings plus bonus is ₹7000. The excess amount will be forfeited.</li>
+            <li class="uk-margin-small-bottom">This offer cannot be used in conjunction with any other M88 Mansion promotions.</li>
+            <li class="uk-margin-small-bottom">All draw bets, void bets, bets placed on Decimal odds below 1.50 (Malay odds 0.50; HK odds 0.50; Indo odds -2.00) and all bets on excluded games will not be included in the calculation for any turnover and Rollover requirement.</li>
+            <li class="uk-margin-small-bottom">Withdrawals will not be processed if the player still has an active rollover. Please contact Customer Service to arrange for a waiver of the rollover requirement.</li>
+            <li class="uk-margin-small-bottom">M88 Mansion reserves the right to change, update, modify, or cancel this promotion at anytime.</li>
+            <li class="uk-margin-small-bottom"><a href="$m88terms_link" target="_blank">M88 Mansion General Terms and Conditions apply.</a></li>
+        </ol>
+
+    </div>
+
+HTML;
+
+
+$text_prizes = 'Prizes';
+$text_viewprizes = 'View Prizes';
+$prize_1 ='I5/16GB/RTX3060 Gaming PC';
+$prize_2 ='Specialist Gloves | Crimson Web Skin';
+$prize_3 ='₹1600 Free Bet';
+$prize_4 ='₹400 Free Bet + $10 Steam Gift Card';
+$prize_5 ='₹400 Free Bet + 335CG Esports Jersey';
+$prize_6 ='₹400 Free Bet + 50 M88 Esports Lottery Tickets';
+$prize_7 ='₹400 Free Bet';
+
